e837e4685d9d735926fa240abf7223e805c99ed9
HRA! <hra@mvj.biglobe.ne.jp>
2022-09-24 22:11:43 +0900

    Updated

--- pico_firmware/megacon_k_type_test/main.cpp
+++ pico_firmware/megacon_k_type_test/main.cpp
@@ -32,61 +32,97 @@
 #include "hardware/gpio.h"
 
 // --------------------------------------------------------------------
-//  Raspberry Pi Pico
-//                ______|~~~~~|______
-//           GP0  |  Åõ |_____| Åõ  | VBUS
-//           GP1  |  [ ]  USB       | VSYS
-//           GND  |  LED        Å°  | GND
-//           GP2  |                 | 3V3_EN
-//           GP3  |  ( )     Å° Å°  | 3V3(OUT)
-//           GP4  |                 | ADC_VREF
-//           GP5  |                 | GP28
-//           GND  |  Å°             | GND
-//           GP6  |                 | GP27
-//           GP7  |  y  Å°Å°Å°      | GP26
-//           GP8  |  r  Å°Å°Å°      | RUN
-//           GP9  |  r  Å°Å°Å°      | GP22
-//           GND  |  e              | GND
-//           GP10 |  b              | GP21
-//           GP11 |  p              | GP20
-//           GP12 |  s              | GP19
-// [/SLTSEL] GP13 |  a              | GP18 [A15]
-//           GND  |  R              | GND
-//     [/RD] GP14 |      DEBUG      | GP17 [A14]
-//           GP15 |  Åõ         Åõ  | GP16 [A13]
-//                ~~~~~~~~~~~~~~~~~~~
+//  Raspberry Pi Pico (Top View)
+//                 ______|~~~~~|______
+//      o[D0] GP0  |  Åõ |_____| Åõ  | VBUS
+//      o[D1] GP1  |  [ ]  USB       | VSYS
+//            GND  |  LED        Å°  | GND
+//      o[D2] GP2  |                 | 3V3_EN
+//      o[D3] GP3  |  ( )     Å° Å°  | 3V3(OUT)
+//      o[D4] GP4  |                 | ADC_VREF
+//      o[D5] GP5  |                 | GP28
+//            GND  |  Å°             | GND
+//      o[D6] GP6  |                 | GP27
+//      o[D7] GP7  |  y  Å°Å°Å°      | GP26
+//            GP8  |  r  Å°Å°Å°      | RUN
+//            GP9  |  r  Å°Å°Å°      | GP22
+//            GND  |  e              | GND
+//            GP10 |  b              | GP21
+//            GP11 |  p              | GP20
+//            GP12 |  s              | GP19 [A15]o
+// o[/SLTSEL] GP13 |  a              | GP18 [A14]o
+//            GND  |  R              | GND
+//     o[/RD] GP14 |      DEBUG      | GP17 [A13]o
+//     o[/WR] GP15 |  Åõ         Åõ  | GP16 [A12]o
+//                 ~~~~~~~~~~~~~~~~~~~
 // --------------------------------------------------------------------
 
 // --------------------------------------------------------------------
 int main( void ) {
-	int address = 0;
+	int address = 0, data = 0;
 	static const int sltsel = 13;
+	static const int wr = 15;
 	static const int rd = 14;
 
+	gpio_init( 0 );
+	gpio_init( 1 );
+	gpio_init( 2 );
+	gpio_init( 3 );
+	gpio_init( 4 );
+	gpio_init( 5 );
+	gpio_init( 6 );
+	gpio_init( 7 );
 	gpio_init( 13 );
 	gpio_init( 14 );
+	gpio_init( 15 );
 	gpio_init( 16 );
 	gpio_init( 17 );
 	gpio_init( 18 );
+	gpio_init( 19 );
+	gpio_set_dir( 0, true );	//	out
+	gpio_set_dir( 1, true );	//	out
+	gpio_set_dir( 2, true );	//	out
+	gpio_set_dir( 3, true );	//	out
+	gpio_set_dir( 4, true );	//	out
+	gpio_set_dir( 5, true );	//	out
+	gpio_set_dir( 6, true );	//	out
+	gpio_set_dir( 7, true );	//	out
 	gpio_set_dir( 13, true );	//	out
 	gpio_set_dir( 14, true );	//	out
+	gpio_set_dir( 15, true );	//	out
 	gpio_set_dir( 16, true );	//	out
 	gpio_set_dir( 17, true );	//	out
 	gpio_set_dir( 18, true );	//	out
+	gpio_set_dir( 19, true );	//	out
 	for(;;) {
-		//	A13-A15
-		gpio_put_masked( 7 << 16, address << 16 );
+		//	/WR phase ---------------------------------------
+		//	A15-A12, D7-D0
+		gpio_put_masked( 0x000F00FF, (address << 16) | data );
+		//	/WR = 0
+		gpio_put( wr, 0 );
+		//	/SLTSEL = 0
+		gpio_put( sltsel, 0 );
+		sleep_us(1);
+		//	/WR = 1
+		gpio_put( wr, 1 );
+		//	/SLTSEL = 1
+		gpio_put( sltsel, 1 );
+		sleep_us(1);
+
+		//	/RD phase ---------------------------------------
+		//	A15-A12, D7-D0
+		gpio_put_masked( 0x000F00FF, (address << 16) | 0xFF );
 		//	/RD = 0
 		gpio_put( rd, 0 );
 		//	/SLTSEL = 0
 		gpio_put( sltsel, 0 );
-		address = (address + 1) & 7;
 		sleep_us(1);
 		//	/RD = 1
 		gpio_put( rd, 1 );
 		//	/SLTSEL = 1
 		gpio_put( sltsel, 1 );
 		sleep_us(1);
+		address = (address + 1) & 15;
 	}
 	return 0;
 }
---
+++ pico_firmware/megacon_k_type_test/readme.txt
@@ -0,0 +1,10 @@
+こちらは、megacon_k_type_compatible の動作確認用の対抗モデルになります。
+megacon_k_type_compatible を書き込んだ Pico とは別に、テスト用治具としてもう一つ Pico を用意して、
+適切に繋げることで、スロット信号を模したテスト信号を発生させ、megacon_k_type_compatible の挙動を
+ロジアナなどで観測することが可能になります。
+
+This is a competing model for checking the operation of megacon_k_type_compatible.
+By preparing another Pico as a test jig and connecting it to the Pico with megacon_k_type_compatible 
+written in it, you can generate a test signal that imitates a slot signal and observe the behavior 
+of megacon_k_type_compatible with a logic analyzer or other device. The behavior of megacon_k_type_compatible 
+can be observed with a logic analyzer or other equipment.
